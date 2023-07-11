4c7466de61c7681d9cbd9fe11af054c50244c30d
Vinícius Alves <viniciusmra@gmail.com>
2023-07-11 16:02:49 -0300

    botões e filtros da api

--- public/script.js
+++ public/script.js
@@ -1,27 +1,29 @@
-var allData
-fetch('https://restcountries.com/v3.1/lang/portuguese')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data[0])
-    for(let i = 0; i < data.length; i++){
-        if(data[i].capital == undefined){
-            continue
+showData();
+function showData(filter = "all") {
+  apiURL = "https://restcountries.com/v3.1/"
+
+  if (filter == "all") {
+    apiURL = apiURL + "/"
+  } else {
+    apiURL = apiURL + "/lang/"
+  }
+  
+  apiURL = apiURL + filter
+  fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("data").innerHTML = "";
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].capital == undefined) {
+          continue
         }
         myDiv = document.createElement("div")
         myDiv.className = "city"
-        myDiv.innerHTML = "<img src=" + data[i].flags.png +"></img> " + data[i].translations.por.common
+        myDiv.innerHTML = "<img src=" + data[i].flags.png + "></img> " + data[i].translations.por.common
         document.getElementById("data").appendChild(myDiv)
-    }
-    //createElement('span')
-
-    
-  })
-  .catch(error => {
-    console.error(error);
-});
-
-
-
-// createElement('span')
-
-// for(let i = 0; i < len())
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
