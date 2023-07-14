1f2e307ce65454927be116506a1c566ab0bf0f47
Vinícius Alves <viniciusmra@gmail.com>
2023-07-14 15:48:26 -0300

    server-side API request

--- public/script.js
+++ public/script.js
@@ -1,36 +1,49 @@
-showData();
-function showData(filter = "all") {
+// Chama a função getData assim que o script é carregado
+getData("all")
+
+
+// Faz uma requisição GET ao servidor passando os parametros que deseja receber como resposta
+function getData(filter){
+  // Verifica os dados solicitados já são os que estão sendo exibidos na tela, para evitar uma nova chamada na api
+  if(document.getElementById(filter).classList.contains("selected")){
+    return
+  }
+
+  // Faz o tratamento do estado dos botões
   selected = document.querySelectorAll(".selected");
   if(selected.length > 0){
     selected[0].classList.remove("selected");
   }
   document.getElementById(filter).classList.add("selected");
-  apiURL = "https://restcountries.com/v3.1"
 
-  if (filter == "all") {
-    apiURL = apiURL + "/"
-  } else {
-    apiURL = apiURL + "/lang/"
+  // Chamada da back-end
+  if(filter != "all"){
+    filter = `lang/${filter}`
   }
-  
-  apiURL = apiURL + filter
-  fetch(apiURL)
+
+  fetch(`/get-data?data_filter=${filter}`)
     .then(response => response.json())
     .then(data => {
-      document.getElementById("data").innerHTML = "";
-      for (let i = 0; i < data.length; i++) {
-        myDiv = document.createElement("div")
-        myDiv.className = "card"
-        myDiv.innerHTML = "<img src=" + data[i].flags.png + "></img> " + data[i].translations.por.common
-        currency = ""
-        if(data[i].currencies != null){
-          currency = " - Currency: " + Object.entries(data[i].currencies)[0][1].name + " (" + Object.entries(data[i].currencies)[0][1].symbol + ")"
-        }
-        myDiv.innerHTML = myDiv.innerHTML + currency
-        document.getElementById("data").appendChild(myDiv)
-      }
+      showData(data) // Chama a função showData
     })
     .catch(error => {
       console.error(error);
     });
+}
+
+// Constroi os elementos da interface gráfica
+function showData(data){
+  document.getElementById("data").innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    myDiv = document.createElement("div")
+    myDiv.className = "card"
+    myDiv.innerHTML = "<img src=" + data[i].flags.png + "></img> " + data[i].translations.por.common
+    currency = ""
+    if(data[i].currencies != null){
+      currency = " - Currency: " + Object.entries(data[i].currencies)[0][1].name + " (" + Object.entries(data[i].currencies)[0][1].symbol + ")"
+    }
+    myDiv.innerHTML = myDiv.innerHTML + currency
+    document.getElementById("data").appendChild(myDiv)
+  }
+
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 var path = require('path');
-
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
+// Indica os arquivos estáticos do servidor que são acessíveis ao cliente
 app.use(express.static(path.join(__dirname,'..', 'public')));
 
-// Iniciar o servidor
+// Faz a chamada a API REST COUNTRIES com os parametros fornecidos pelo front-end e retorna os dados solicitados
+app.get('/get-data', async (req, res) => {
+  const {data_filter} = req.query;
+  console.log(`Foi recebida uma solicitação para: ${data_filter}, solicitando dados para API`)
+  try {
+    const response = await fetch(`https://restcountries.com/v3.1/${data_filter}`);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter os dados da API externa' });
+  }
+});
+
+// Inicia o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
